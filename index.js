ce8deed60e81c55704b013cfc1522b8953f1c107
Your Name <you@example.com>
2021-05-15 17:21:53 +0530

    project upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function updatemap() {
+    console.log("updating map with real time data");
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            //console.log(rsp.data);
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+                let cases=element.infected;
+                if(cases>255)
+                {
+                    color= "rgb(255,0,0)";
+                }
+
+                else
+                {
+                    color = `rgb(${cases},0,0)`;
+                }
+
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
+        });
+}
+let interval=2000;
+setInterval(updatemap,interval);
